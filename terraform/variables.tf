37e366f6d9376d63adc43c994ffe2469124f1ccc
Kartik Paliwal <kartikp752003@gmail.com>
2025-09-23 00:36:24 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,15 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
+
+variable "aws_access_key_id" {
+  description = "AWS access key for ECR login"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret key for ECR login"
+  type        = string
+  sensitive   = true
+}
